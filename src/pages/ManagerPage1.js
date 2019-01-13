4b95a6c4890fda934c02fe141a3f16a7f415c091
Rebecca-yh <1652818@tongji.edu.cn>
2019-01-13 14:03:43 +0800

    finish

--- src/pages/ManagerPage1.js
+++ src/pages/ManagerPage1.js
@@ -4,21 +4,40 @@ import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Card from '@material-ui/core/Card';
-
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import Charts from '../component/charts'
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
-
-
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-
+import echarts from 'echarts/lib/echarts';
+import ReactEcharts from 'echarts-for-react';
+import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 import ManagerDrawer from '../component/ManagerDrawer'
 const drawerWidth = 240;
+const currencies = [
+    {
+        value: '0',
+        label: '已完成',
+    },
+    {
+        value: '1',
+        label: '未支付',
+    },
+    {
+        value: '2',
+        label: '已取消',
+    },
 
+];
 const styles = theme => ({
     root: {
         display: 'flex',
     },
+    map:
+        {
+            height:700,
+        },
     card: {
         height:700,
     },
@@ -41,9 +60,22 @@ const styles = theme => ({
 });
 
 class ManagerPage1 extends React.Component {
+
     state = {
         open: false,
+        startDate: '',
+        endDate: '',
+        status:''
     };
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            startDate: new Date(),
+            endDate: new Date()
+        };
+
+    }
 
     handleDrawerOpen = () => {
         this.setState({ open: true });
@@ -51,6 +83,97 @@ class ManagerPage1 extends React.Component {
 
     handleDrawerClose = () => {
         this.setState({ open: false });
+    };
+    handleChange=(currencies)=>
+    {
+        this.setState({
+            status: currencies
+        });
+    };
+    handleStartDateChange=(date)=> {
+        this.setState({
+
+            startDate: date
+        });
+    };
+    handleEndDateChange=(date) =>{
+        this.setState({
+            endDate: date
+        });
+    };
+    handleCommit=() =>{
+        var start=this.state.startDate;
+        var end=this.state.endDate;
+        var status=this.state.status;
+
+        var url='http://localhost:8080/admin/addCount?';
+        url+='start=';
+        url+=this.dateToString(start);
+        url+='&end=';
+        url+=this.dateToString(end);
+        url+='&status=';
+        url+='1';
+        console.log(url);
+        var myChart = echarts.init(document.getElementById('charts'));
+
+        axios.get(url,{})
+            .then((response) =>{
+                console.log(JSON.stringify(response.data));
+                var list=response.data.data;
+                console.log(list);
+                var date=[];
+                var count=[];
+                list.forEach(function (value, list) {
+                    console.log(value);
+                    console.log(value.date);
+                    console.log(value.count);
+                    date.push(value.date);
+                    count.push(value.count)
+
+                });
+                myChart.setOption({
+                    title: {
+                        text: '订单统计'
+                    },
+                    tooltip: {},
+                    legend: {
+                        data:['销量']
+                    },
+                    xAxis: {
+                        data: date
+                    },
+                    yAxis: {},
+                    series: [{
+                        name: '订单数',
+                        type: 'line',
+                        data: count
+                    }]
+                });
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+
+
+
+
+    };
+    dateToString(date){
+      console.log(date.toString());
+        var year = date.getFullYear().toString();
+        var month =(date.getMonth() + 1).toString();
+        var day = (date.getDate()).toString();
+        if (month.length == 1) {
+            month = "0" + month;
+        }
+        if (day.length == 1) {
+            day = "0" + day;
+        }
+        var dateTime = year + "-" + month + "-" + day;
+        return dateTime;
+
     };
 
     render() {
@@ -70,45 +193,73 @@ class ManagerPage1 extends React.Component {
                     <Grid
                         container
                         spacing={8}
-                        direction="row"
+
                         alignItems="center"
                         justify="center"
                         style={{ minHeight: '100vh' }}
                     >
-                        <Grid item xs={3}>
+                        <Grid item xs={12} sm={9}>
+                            <Card className={classes.map}>
+                                <CardContent>
+                                    <div id="charts" style={{ width: 600, height: 600 }}></div>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={12} sm={3}>
                             <Card className={classes.card}>
                                 <CardContent>
                                     <form >
-
-                                        <TextField
-                                            id="startDate"
-                                            label="起始日期"
-                                            type="date"
-                                            defaultValue="2017-05-24"
-                                            className={classes.textField}
-                                            InputLabelProps={{
-                                                shrink: true,
-                                            }}
-                                        />
-                                        <TextField
-
-                                            id="startDate"
-                                            label="结束日期"
-                                            type="date"
-                                            defaultValue="2017-05-24"
-                                            className={classes.textField}
-                                            InputLabelProps={{
-                                                shrink: true,
-                                            }}
+                                        <div>
+                                            统计总订单
+                                        </div>
+                                        <br/>
+                                        <font size="2">开始时间</font>
+                                        <br/>
+                                        <DatePicker
+                                            selected={this.state.startDate}
+                                            onChange={this.handleStartDateChange}
 
                                         />
+                                    <br/>
+                                        <br/>
+                                        <font size="2">结束时间</font>
+                                        <br/>
+                                        <DatePicker
+                                            selected={this.state.endDate}
 
+                                            onChange={this.handleEndDateChange}
+
+                                        />
+                                        <br/>
+
+                                        <TextField
+                                            id="standard-select-currency-native"
+                                            select
+                                            label="订单状态"
+                                            className={classes.textField}
+                                            value={this.state.currency}
+                                            onChange={this.handleChange}
+                                            SelectProps={{
+                                                native: true,
+                                                MenuProps: {
+                                                    className: classes.menu,
+                                                },
+                                            }}
+
+                                            margin="normal"
+                                        >
+                                            {currencies.map(option => (
+                                                <option key={option.value} value={option.value}>
+                                                    {option.label}
+                                                </option>
+                                            ))}
+                                        </TextField>
                                         <Button
-                                            type="submit"
+
                                             fullWidth
                                             variant="contained"
                                             color="primary"
-
+                                            onClick={this.handleCommit}
                                         >
                                             查询
                                         </Button>
@@ -117,13 +268,7 @@ class ManagerPage1 extends React.Component {
                             </Card>
                         </Grid>
 
-                        <Grid item xs={9}>
-                            <Card className={classes.card}>
-                                <CardContent>
-                                  <img src="https://wx3.sinaimg.cn/crop.0.577.2048.1150.1000/971cb82fgy1fms8rtbi9pj21kw2gawvj.jpg"></img>
-                                </CardContent>
-                            </Card>
-                        </Grid>
+
 
                     </Grid>
                 </main>
